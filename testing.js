c98478886c26564249ebaf42851239650f69ce36
anabelen19891994 <ana.b.coronel@outlook.com>
2024-03-19 14:02:25 -0300

    Se agrega el proyecto

---
+++ testing.js
@@ -0,0 +1,22 @@
+const ProductManager = require("./main");
+
+const producto = new ProductManager();
+
+// console.log(producto.addProduct('televisión','samsung',1200000,'img1','abc123',10));
+// console.log(producto.addProduct('heladera','samsung',1365000,'img2','def123',5));
+// console.log(producto.addProduct('heladera','samsung',1365000,'img2','ghi123',5));
+
+// console.log(producto.getProducts());
+// console.log(producto.getProductById(1));
+
+// console.log(producto.deleteProduct(3));
+
+const productoActualizar = {
+  id: 30,
+  title: "heladera 1",
+  description: "samsung 2",
+  price: 136500,
+  thumbnail: "img20",
+};
+
+console.log(producto.updateProduct(3, productoActualizar));
